c07de93a9f24c4775ad96efecdcb5aaeb83f15f1
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-23 15:27:33 +0100

    Fix directory path in install-from-github.sh

--- install-fullkde-grub/install-from-github.sh
+++ install-fullkde-grub/install-from-github.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/$USER && git clone https://github.com/claudemods/vanillaarch-to-cachyos && cd install-fullkde-grub && ./install-repo-and-full-cachyos-kde-grub-packages.sh
+cd /home/$USER && git clone https://github.com/claudemods/vanillaarch-to-cachyos && cd /home/$USER/vanillaarch-to-cachyos/install-fullkde-grub && ./install-repo-and-full-cachyos-kde-grub-packages.sh
