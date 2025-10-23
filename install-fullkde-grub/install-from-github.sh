a29545f7b1645e0242c300b183802f2825baeca3
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-23 15:26:33 +0100

    Simplify installation script with chained commands

--- install-fullkde-grub/install-from-github.sh
+++ install-fullkde-grub/install-from-github.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-git clone https://github.com/claudemods/vanillaarch-to-cachyos
-cd install-fullkde-grub
-./install-repo-and-full-cachyos-kde-grub-packages.sh
+cd /home/$USER && git clone https://github.com/claudemods/vanillaarch-to-cachyos && cd install-fullkde-grub && ./install-repo-and-full-cachyos-kde-grub-packages.sh
