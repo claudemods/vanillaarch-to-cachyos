7ccc83286a61ede4e5e5d860496570e8869cc91a
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-24 19:24:10 +0100

    broken not working

--- c++/script/readme.md
+++
@@ -1,5 +0,0 @@
-to compile install gcc
-note i will not be embedding the extra needed files such as config.fish in this version using the c++ qrc option 
-
-and use command 
-g++ -std=c++23 -Wall -Wextra -o archconv+ main.cpp
--- c++/script/setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cd /home/$USER && git clone https://github.com/claudemods/vanillaarch-to-cachyos && cd /home/$USER/vanillaarch-to-cachyos/c++/script && g++ -std=c++23 -Wall -Wextra -o archconv+ main.cpp && sudo chmod +x archconv+ && ./archconv+ && sudo rm -rf https://github.com/claudemods/vanillaarch-to-cachyos
