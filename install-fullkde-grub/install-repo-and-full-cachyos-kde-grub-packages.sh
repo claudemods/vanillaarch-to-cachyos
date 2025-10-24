50f83fdf7daa4651fc5fb31c26d8cbd05733d552
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-24 21:11:01 +0100

    cachyos kde theme added

--- install-fullkde-grub/install-repo-and-full-cachyos-kde-grub-packages.sh
+++ install-fullkde-grub/install-repo-and-full-cachyos-kde-grub-packages.sh
@@ -1273,6 +1273,7 @@ print_status "Fish configuration applied"
 
 print_info "Apply Cachyos Kde Theme..."
 sudo chmod +x /home/$USER/vanillaarch-to-cachyos/install-fullkde-grub/installcachyostheme.sh
+sudo chmod +x /home/$USER/vanillaarch-to-cachyos/install-fullkde-grub/start.sh
 ./installcachyostheme.sh
 print_info "Theme Applied..."
 
