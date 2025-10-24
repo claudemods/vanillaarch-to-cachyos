393ce67ec472199ebed5e6d9f7f42353e8a0c008
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-24 21:48:29 +0100

    Update start.sh

--- install-fullkde-grub/start.sh
+++ install-fullkde-grub/start.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 nohup /usr/bin/plasmashell
-print_section "CachyOS Conversion Complete!"
-echo -e "${GREEN}${BOLD}"
-echo "Conversion to CachyOS has been completed successfully!"
-sudo rm -rf /home/$USER/vanillaarch-to-cachyos
-echo "Please reboot your system to apply all changes."
-echo -e "${NC}"
